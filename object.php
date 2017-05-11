17923cbfc9e834a22d78a300b50da739c285d38f
Artur Stępień <artur.stepien@bestproject.pl>
2017-05-11 17:07:31 +0200

    Adding Object tratir with basic object methods

---
+++ object.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace BestProject\Wordpress\Object;
+
+/**
+ * Just a basic structure for basic class methods.
+ */
+trait Object
+{
+
+	/**
+	 * Get a given property.
+	 *
+	 * @param	String	$property		Name of a property to get.
+	 * @param	Mixed	$default_value	Default value to return in case property was emtpy or not found.
+	 * @return	Mixed
+	 */
+	public function get($property, $default_value = null)
+	{
+
+		if (isset($this->$property)) {
+			return $this->$property;
+		} else {
+			return $default_value;
+		}
+	}
+
+	/**
+	 * Sets a given property.
+	 *
+	 * @param	Strong	$property	A property to set.
+	 * @param	Mixed	$value		Value to set.
+	 *
+	 * @throws ErrorException
+	 */
+	public function set($property, $value = true)
+	{
+		$className = get_called_class();
+
+		if (property_exists($this, $property)) {
+			$this->$property = $value;
+		} else {
+			throw new ErrorException('Property <b>'.$property.'</b> does not exists in <b>'.$className.'</b>',
+			500);
+		}
+	}
+}
