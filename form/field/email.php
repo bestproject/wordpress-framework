038aa6dac746e76c877c53b40912b7b215ca8ae3
Artur Stępień <artur.stepien@bestproject.pl>
2017-05-13 23:33:00 +0200

    Fixed e-mail validation

--- form/field/email.php
+++ form/field/email.php
@@ -20,23 +20,38 @@ class Email extends Field
 	{
 		$id			 = (!empty($this->id) ? ' id="'.$this->id.'"' : '');
 		$value		 = (!empty($this->value) ? ' value="'.$this->value.'"' : '');
-		$hint		 = (!empty($this->hint) ? ' placeholder="'.Language::_($this->hint).'"' : '');
+		$hint		 = (!empty($this->hint) ? ' placeholder="'.Language::_($this->hint).'"'
+				: '');
 		$class		 = (!empty($this->class) ? ' class="'.$this->class.'"' : '');
 		$required	 = ($this->required ? ' required' : '');
 		?><input type="email" name="<?php echo $this->name ?>"<?php echo $id ?><?php echo $value ?><?php echo $class ?><?php echo $hint ?><?php echo $required ?> /><?php
 	}
 
 	/**
-	 * Validate this field.
+	 * Validate this field. Returns TRUE on success or error message on failure.
 	 * 
 	 * @return	Boolean
 	 */
-	public function validate() {
+	public function validate()
+	{
 
-		if( !parent::validate() OR filter_var($this->getSaveData(), FILTER_VALIDATE_EMAIL)===false ) {
-			return false;
+		// Check input email
+		$valid_email = !(filter_var($this->getSaveData(), FILTER_VALIDATE_EMAIL) === false);
+
+		// Check parent validation value
+		$validation_result = parent::validate();
+
+		// If this field failed parent validation, return error message.
+		if ($validation_result !== true) {
+			return $validation_result;
 		}
 
+		// If this field has an invalid e-mail return error
+		if (!$valid_email) {
+			return Language::_('FORM_EMAIL_FIELD_WRONG_VALUE', Language::_($this->label));
+		}
+
+		// All Ok so return a success.
 		return true;
 	}
 }
